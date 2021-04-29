e445cb4974f068b066689019b1834caef68e362b
Merz <1318038346@qq.com>
2021-04-29 23:20:11 +0800

    111

--- src/components/IndexBar.vue
+++ src/components/IndexBar.vue
@@ -3,7 +3,7 @@
     <div v-for="value in data">
       <van-index-anchor :index="value.layerModel"/>
       <van-grid square>
-        <van-grid-item :to="{path: '/carClass',query: {id: obj.id}}" v-for="obj in value.carListEntitys" :key="obj.id" icon="https://gimg2.baidu.com/image_search/src=http%3A%2F%2Fimg.zcool.cn%2Fcommunity%2F0128dd5568b40500000127163893ba.jpg%402o.jpg&refer=http%3A%2F%2Fimg.zcool.cn&app=2002&size=f9999,10000&q=a80&n=0&g=0n&fmt=jpeg?sec=1622273997&t=6372468d3dd093a0bcacd941cedf04f3" :text="obj.name" />
+        <van-grid-item :to="{path: '/carClass',query: {id: obj.id}}" v-for="obj in value.carListEntitys" :key="obj.id" :icon="obj.img" :text="obj.name" />
       </van-grid>
     </div>
   </van-index-bar>
